0d7a0b172dec299f97aabf563b4cde159c115c1c
Jan Helge Aalbu <31095620+jhaalbu@users.noreply.github.com>
2023-11-19 21:22:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ def calculate_energy(mass, height, gravity=9.81):
 # Streamlit application
 st.title('Rock Fall Energy Calculator')
 mass = st.number_input('Gi vekt av stein (i kg):', min_value=0, value=1000)
-height = st.number_input('Gi høgden den fell i fritt fall (i meters):', min_value=1, value=50)
+height = st.number_input('Gi høgden den fell i fritt fall (i meter):', min_value=1, value=50)
 
     # Button to perform calculation
 if st.button('Calculate Energy'):
   energy = calculate_energy(mass, height)
-  st.write(f'Energien steine har når den treff er: {energy} joule.')
+  st.write(f'Energien steien har når den treff er: {energy} kJ.')
